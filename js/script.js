7afc932981da886746f6d36f59f84117290b8e51
alunoEtec <aula@etec.com>
2025-11-27 20:30:14 -0300

    atualizando js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const pokemonName = document.querySelector('.pokemon__name');
+
+const fetchpokemon = async (pokemon) => {
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+    const data = await APIResponse.json();
+    return data;
+}
+
